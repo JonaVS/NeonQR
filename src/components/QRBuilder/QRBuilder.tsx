d320844a915a18f59a182c7768d51facbf9269a0
JonaVS <jon4963@gmail.com>
2023-01-07 18:43:22 -0600

    refactor: turn wrongly rendered text at QRBuilder into comment

--- src/components/QRBuilder/QRBuilder.tsx
+++ src/components/QRBuilder/QRBuilder.tsx
@@ -13,11 +13,11 @@ const QRBuilder = () => {
       boxShadow="0 0 15px #000000, 0 0 15px #000000"
       rounded="lg"
     >
-      /*
+      {/*
         The div is needed to export the Code as IMG. 
         For some reason if the div is inside the QRcode component 
         the export generates wrong content.
-      */
+      */}
       <chakra.div id="to-img-target" p={5}>
         <QRCode />
       </chakra.div>
