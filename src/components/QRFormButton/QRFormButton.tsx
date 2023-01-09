ce594ca9e713f0a10c1c3fb252e040c2f62e9395
JonaVS <jon4963@gmail.com>
2023-01-09 14:23:23 -0600

    feat: add animation to QRFormButton

--- src/components/QRFormButton/QRFormButton.tsx
+++ src/components/QRFormButton/QRFormButton.tsx
@@ -1,25 +1,34 @@
 import React from "react";
-import { Button, ButtonProps } from "@chakra-ui/react";
+import { ButtonProps, chakra } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
-type Props = ButtonProps 
+type Props = ButtonProps;
 
-const QRFormButton = (props:Props) => {
+//START--Framer motion variants--START
+const toPngBtn = {
+  tap: {
+    scale: 0.9,
+    transition: { duration: 0.2, type: "spring" },
+  },
+};
+//END--Framer motion variants--END
 
+const QRFormButton = (props: Props) => {
   return (
-    <Button
+    <chakra.button
+      as={motion.button}
+      variants={toPngBtn}
+      whileTap={toPngBtn.tap}
       backgroundColor="#8c00ff"
       minW="36"
       alignSelf="center"
       boxShadow="0 0 5px #8c00ff, 0 0 10px #8c00ff"
-      _hover={{
-        backgroundColor: "transparent",
-        boxShadow: "0 0 5px #00ffff, 0 0 10px #00ffff",
-        textShadow: "0 0 5px #8c00ff, 0 0 10px #8c00ff",
-      }}
+      p={2}
+      rounded="md"
       {...props}
     >
       Get as PNG
-    </Button>
+    </chakra.button>
   );
 };
 
