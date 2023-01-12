7c24f5c30520cfcd7949ad51ce9623c49d36b2eb
JonaVS <jon4963@gmail.com>
2023-01-11 18:44:24 -0600

    refactor: remove unnecessary QRCode styles

--- src/components/QRCode/QRCode.styles.tsx
+++ src/components/QRCode/QRCode.styles.tsx
@@ -10,11 +10,8 @@ export const StyledQRCodeSVG = styled(QRCodeSVG)<Props>`
   filter: ${(props) => props.glow && "url(#f2) brightness(1.1) saturate(1.1)"}; 
   transform:  translateZ(0);
   -webkit-transform: translateZ(0);
-  path:nth-of-type(1) {
-    fill: ${(props) => props.bgColor || "black"};
-  }
   path:nth-of-type(2) {
-    fill: ${(props) => props.color || "cyan"};
+    fill: ${(props) => props.color};
     filter: ${(props) => props.glow && "url(#f1)"};
   }
 `;
