00d6bd8d14576af1fecf26c1dbc56e9b6c8cd57d
JonaVS <jon4963@gmail.com>
2023-01-15 15:19:46 -0600

    refactor: use Chakra sx prop for custom styles at ColorInput

--- src/components/QRFormColorInput/QRFormColorInput.tsx
+++ src/components/QRFormColorInput/QRFormColorInput.tsx
@@ -1,10 +1,38 @@
 import React from "react";
-import { StyledColorInput } from "./QRFormColorInput.styles";
+import { Input, InputProps } from "@chakra-ui/react";
 
-type Props = React.ComponentPropsWithoutRef<"input"> 
+const sharedInputStyles = {
+  borderRadius: "50%",
+  boxShadow: "0 0 10px #757575",
+  border: "none",
+};
 
-const QRFormColorInput = (props:Props) => {
-    return <StyledColorInput type='color' {...props}/>
-}
+type Props = InputProps
 
-export default QRFormColorInput
+const QRFormColorInput = (props: Props) => {
+  return (
+    <Input
+      sx={{
+        webkitAppearance: "none",
+        "::-webkit-color-swatch": {
+          ...sharedInputStyles,
+        },
+        "::-moz-color-swatch": {
+          ...sharedInputStyles,
+        },
+      }}
+      margin={0}
+      padding={0}
+      appearance="none"
+      width="40px"
+      height="40px"
+      backgroundColor="transparent"
+      border="none"
+      cursor="pointer"
+      type="color"
+      {...props}
+    />
+  );
+};
+
+export default QRFormColorInput;
